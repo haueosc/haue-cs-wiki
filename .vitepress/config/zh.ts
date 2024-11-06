05c9f7b91fc72a2de095badd708d57ff4d0746ac
Doge2077 <1002302241@qq.com>
2024-11-06 10:16:32 +0800

    feat:init main

---
+++ .vitepress/config/zh.ts
@@ -0,0 +1,218 @@
+import { defineConfig, type DefaultTheme } from 'vitepress'
+import { forewordIcon, envIcon, grammarIcon, algoIcon, linuxIcon } from '../theme/icons/svg-path'
+
+export const zh = defineConfig({
+    lang: 'zh-Hans',
+    title: "HAUE-CS-WIKI",
+    description: "河工计算机学习指南",
+    themeConfig: {
+        //顶部导航栏选项
+        nav: nav(),
+
+        //侧边导航栏选项
+        sidebar: sidebar(),
+
+        editLink: {
+            pattern: 'https://github.com/haueosc/haue-cs-wiki/edit/dev-vitepress/docs/:path',
+            text: '在 GitHub 上编辑此页面'
+        },
+
+        docFooter: {
+            prev: '上一页',
+            next: '下一页'
+        },
+
+        outline: {
+            label: '页面导航',
+            level: 'deep'
+        },
+
+        lastUpdated: {
+            text: '最后更新于',
+            formatOptions: {
+                dateStyle: 'short',
+                timeStyle: 'medium'
+            }
+        },
+
+        langMenuLabel: '多语言',
+        returnToTopLabel: '回到顶部',
+        sidebarMenuLabel: '菜单',
+        darkModeSwitchLabel: '主题',
+        lightModeSwitchTitle: '切换到浅色模式',
+        darkModeSwitchTitle: '切换到深色模式'
+    }
+})
+
+function nav(): DefaultTheme.NavItem[] {
+    return [
+        { text: '首页', link: '/Main' },
+        { text: 'Hello 算法', link: 'https://www.hello-algo.com/' },
+        { text: 'Oi Wiki', link: 'https://oi-wiki.org/' },
+        { text: 'HAUE OJ', link: 'http://www.haueacm.top/' },
+    ]
+}
+
+function sidebar(): DefaultTheme.SidebarItem[] {
+    return [
+        {
+            text: forewordIcon + '序言',
+            collapsed: false,
+            items: [
+                { text: '编写初衷', link: '/Foreword/intention', },
+                { text: '入门须知', link: '/Foreword/prerequisites', },
+            ]
+        },
+        {
+            text: envIcon + '环境搭建',
+            collapsed: false,
+            items: [
+                { text: '引入', link: '/Environment/introduction' },
+                { text: '准备', link: '/Environment/pre-knowledges' },
+                {
+                    text: 'C/C++',
+                    collapsed: true,
+                    items: [
+                        { text: '概述', link: '/Environment/c-cpp/overview' },
+                        { text: 'Dev C++', link: '/Environment/c-cpp/bloodshed-devcpp' },
+                        { text: '小熊猫Dev C++', link: '/Environment/c-cpp/red-panda' },
+                        { text: 'CodeBlocks', link: '/Environment/c-cpp/codeblocks' },
+                        { text: 'Visual Studio', link: '/Environment/c-cpp/visual-studio' },
+                        { text: 'Visual Studio Code', link: '/Environment/c-cpp/vs-code' },
+                        { text: 'Sublime Text', link: '/Environment/c-cpp/sublime-text' },
+                        { text: 'Vim/Neovim', link: '/Environment/c-cpp/vim-neovim' },
+                    ]
+                },
+                {
+                    text: 'Java',
+                    collapsed: true,
+                    items: [
+                        { text: '概述', link: '/Environment/java/overview' },
+                        { text: 'Eclipse', link: '/Environment/java/eclipse' },
+                        { text: 'IntelliJ IDEA', link: '/Environment/java/idea' },
+                        { text: 'Visual Studio Code', link: '/Environment/java/vs-code' },
+                        { text: 'Sublime Text', link: '/Environment/java/sublime-text' },
+                        { text: 'Vim/Neovim', link: '/Environment/java/vim-neovim' },
+                    ]
+                },
+                {
+                    text: 'Python',
+                    collapsed: true,
+                    items: [
+                        { text: '概述', link: '/Environment/python/overview' },
+                        { text: 'Thonny', link: '/Environment/python/thonny' },
+                        { text: 'PyCharm', link: '/Environment/python/pycharm' },
+                        { text: 'Python IDLE', link: '/Environment/python/py-idle' },
+                        { text: 'Visual Studio Code', link: '/Environment/python/vs-code' },
+                        { text: 'Sublime Text', link: '/Environment/python/sublime-text' },
+                        { text: 'Vim/Neovim', link: '/Environment/python/vim-neovim' },
+                    ]
+                },
+            ]
+        },
+        {
+            text: grammarIcon + '语法基础&基础数据结构',
+            collapsed: false,
+            items: [
+                { text: '引入', link: '/Grammar-DataStructure/introduction', },
+                {
+                    text: 'C语言',
+                    collapsed: true,
+                    items: [
+                        { text: '概述', link: '/Grammar-DataStructure/c/overview' },
+                        { text: '数据类型', link: '/Grammar-DataStructure/c/data-type' },
+                        { text: '运算符', link: '/Grammar-DataStructure/c/operator' },
+                        { text: '流程控制', link: '/Grammar-DataStructure/c/control' },
+                        { text: '函数', link: '/Grammar-DataStructure/c/function' },
+                        { text: '数组', link: '/Grammar-DataStructure/c/array' },
+                        { text: '指针', link: '/Grammar-DataStructure/c/pointer' },
+                        { text: '结构体和链表', link: '/Grammar-DataStructure/c/struct' }
+                    ]
+                },
+                {
+                    text: 'C++',
+                    collapsed: true,
+                    items: [
+                        { text: '概述', link: '/Grammar-DataStructure/cpp/overview' },
+                        { text: '基础语法', link: '/Grammar-DataStructure/cpp/base' },
+                        { text: '输入输出', link: '/Grammar-DataStructure/cpp/in-out' },
+                        { text: '函数', link: '/Grammar-DataStructure/cpp/function' },
+                        { text: '数组与字符串', link: '/Grammar-DataStructure/cpp/array-str' },
+                        { text: '指针与引用', link: '/Grammar-DataStructure/cpp/pointer' },
+                        { text: '内存管理', link: '/Grammar-DataStructure/cpp/memory' }
+                    ]
+                },
+                {
+                    text: 'Python',
+                    collapsed: true,
+                    items: [
+                        { text: '概述', link: '/Grammar-DataStructure/python/overview', },
+                    ]
+                },
+                {
+                    text: 'Java',
+                    collapsed: true,
+                    items: [
+                        { text: '概述', link: '/Grammar-DataStructure/java/overview', },
+                        { text: '初步体验', link: '/Grammar-DataStructure/java/primer', },
+                        { text: '注释', link: '/Grammar-DataStructure/java/annotation', },
+                        { text: '变量与运算符', link: '/Grammar-DataStructure/java/var-opt', },
+                        { text: '数组', link: '/Grammar-DataStructure/java/array', },
+                        {
+                            text: '面向对象',
+                            collapsed: true,
+                            items:
+                                [
+                                    { text: '概述&面向对象三特性', link: '/Grammar-DataStructure/java/OOP', },
+                                    { text: '类与对象', link: '/Grammar-DataStructure/java/ClassAndObject', },
+                                    { text: '关键字', link: '/Grammar-DataStructure/java/keyword', }
+                                ]
+                        },
+
+                        { text: '多线程', link: '/Grammar-DataStructure/java/multithreading', },
+                        { text: '线程池', link: '/Grammar-DataStructure/java/threadPoll', },
+                    ]
+                },
+            ]
+        },
+        {
+            text: algoIcon + '算法基础',
+            collapsed: false,
+            items: [
+                { text: '引入', link: '/Algorithm/introduction' },
+                { text: 'Hello Algo' },
+                { text: 'Oi Wiki' },
+            ]
+        },
+        {
+            text: linuxIcon + 'Linux初阶',
+            collapsed: false,
+            items: [
+                { text: '引入', link: '/Linux/introduction' },
+                { text: '安装', link: '/Linux/install' },
+                { text: '包管理器', link: '/Linux/package-mgr' },
+            ]
+        },
+    ]
+}
+
+export const search: DefaultTheme.LocalSearchOptions['locales'] = {
+    root: {
+        translations: {
+            button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+            },
+            modal: {
+                displayDetails: '显示详细信息',
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                    selectText: '选择',
+                    navigateText: '切换',
+                    closeText: '关闭',
+                }
+            }
+        }
+    }
+}
--- docs/index.md
+++ docs/index.md
@@ -1,25 +1,34 @@
 ---
-# https://vitepress.dev/reference/default-theme-home-page
 layout: home
 
 hero:
   name: "HAUE-CS-WIKI"
   text: "河工计算机学习指南"
-  tagline: My great project tagline
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
+      text: ✨Getting Started
+      link: /main
     - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: 🔥Java 交流群
+      link: http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=Zm8MUF5_X_NttegYnhdv9eeYxv2iC7sn&authKey=rwPlDN5zUbwU8WzOSvv%2FR3u8wwgS2yu7DAvFAq%2BMRATIglis8SNHg3%2FCLhW%2Bpi8I&noverify=0&group_code=907625834
+    - theme: alt
+      text: Python 交流群
+      link: http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=V4RfyZcFpEw-wfnX-Ck6c0Q8a1NsHK2T&authKey=YK5doMcM43LIIDb8dFkcmymd9g7QThEB9q89m604hVq7VI6v69jSRIfmfQSrN6EE&noverify=0&group_code=476586332
+    - theme: alt
+      text: C++ 交流群
+      link: http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=wK6lTceTXsM1ZHeWWo4i9gfiIy7YFvNz&authKey=O1BE3SHj%2FlfR%2BIo%2Bf9wsVWPl3Gd4YUDzRqk7IehjGpD4NZuMK2aKQ50Q3EcQMCc2&noverify=0&group_code=907733709
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - icon: 📖
+    title: 学科课程，免费精选
+    details: 河工计算机系相关学科课程的资源推荐和学习建议，拒绝选择困难，全套资源均免费获取，放心食用。
+  - icon: 💯
+    title: 考研经验，冲击名校
+    details: 考研上岸985、211的学长学姐们的宝贵经验分享，择校技巧，复习计划，打破信息差。
+  - icon: ⚒️
+    title: 工作经验，冲击大厂
+    details: 大厂学长学姐的找工作经验分享，简历编写，找工作技巧，薪资拆解，你想了解的这里都有。
+  - icon: 💼
+    title: 考公留学，成功案例
+    details: 选调 or 公务员，留学 or 考研，学长学姐成功案例助力你的战略规划。
 ---
-
